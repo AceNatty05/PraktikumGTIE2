d83b5c851474bf2847892bc523aeb266796f6495
Fauzan A <148610901+pojann26@users.noreply.github.com>
2026-03-02 21:48:52 +0700

    Salah orang tadi, sorry men🙏

--- Praktikum1/Keaktifan/24060124140137_Farhan Dwiyan Akbar/tugas-kotak5.cpp
+++ Praktikum1/Keaktifan/24060124140137_Farhan Dwiyan Akbar/tugas-kotak5.cpp
@@ -1,78 +1,35 @@
-/* 
-Nama: Muhammad Fauzan Akbar
-NIM: 24060124140139
-*/
+#include <GL/glut.h>
 
-#include <gl/glut.h>
-
-void gambarSatuKotak(float x, float y, float size) {
-    glBegin(GL_QUADS);
-        glColor3f(0.55f, 0.27f, 0.07f);
-        glVertex2f(x, y);
-        glVertex2f(x + size, y);
-        glVertex2f(x + size, y + size);
-        glVertex2f(x, y + size);
-    glEnd();
-
-    glLineWidth(2.0f);
-    glBegin(GL_LINE_LOOP);
-        glColor3f(1.0f, 0.99f, 0.82f);
-        glVertex2f(x, y);
-        glVertex2f(x + size, y);
-        glVertex2f(x + size, y + size);
-        glVertex2f(x, y + size);
-    glEnd();
-}
-
-void gambarbilah(float x, float y, float size) {
-    glBegin(GL_QUADS);
-        glColor3f(0.75f, 0.75f, 0.75f);
-        glVertex2f(x, y);
-        glVertex2f(x + size, y);
-        glVertex2f(x + size, y + size);
-        glVertex2f(x, y + size);
-    glEnd();
-
-    glLineWidth(2.0f);
-    glBegin(GL_LINE_LOOP);
-        glColor3f(1.0f, 0.99f, 0.82f);
-        glVertex2f(x, y);
-        glVertex2f(x + size, y);
-        glVertex2f(x + size, y + size);
-        glVertex2f(x, y + size);
-    glEnd();
-}
-
-void display() {
+void SegiEmpat5(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     
-    float s = 0.25f;
-
-    // batang
-    gambarSatuKotak( 0.100f,  -0.50f, s);
-    gambarSatuKotak( -0.4f,  -0.50f, s);
-    gambarSatuKotak(-0.150f, -0.50f, s); 
-    gambarSatuKotak(-0.150f, -0.75f, s);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRectf(-0.4, -0.4, -0.2, -0.2);
     
-    // bilah
-    gambarbilah( -0.150f, -0.25f, s);
-    gambarbilah( -0.150f, 0.0f, s);
-    gambarbilah( -0.150f, 0.25f, s);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glRectf(-0.2, -0.4, 0.0, -0.2);
     
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glRectf(0.0, -0.20, 0.2, -0.4);
     
-
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glRectf(0.0, 0.0, 0.2, -0.2);
+    
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRectf(0.0, 0.0, -0.2, -0.2);
+    
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glRectf(0.0, 0.0, 0.2, 0.2);
     glFlush();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
-    glutInitWindowSize(600, 600);
+    glutInitWindowSize(640, 480);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutCreateWindow("Kreasi min 5 Kotak");
-    
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glutDisplayFunc(display);
-    
+    glutCreateWindow("Segi Empat Lima");
+    glutDisplayFunc(SegiEmpat5);
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
     glutMainLoop();
     return 0;
 }
