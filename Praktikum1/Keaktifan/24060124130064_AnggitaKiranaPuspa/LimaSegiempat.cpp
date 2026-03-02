0e069f7117b7af57df8e2b75f926bc1bfff98741
Anggita Kirana Puspa <anggitakiranapuspa2005@gmail.com>
2026-03-02 16:21:16 +0700

    Fix indentation in LimaSegiempat.cpp

--- Praktikum1/Keaktifan/24060124130064_AnggitaKiranaPuspa/LimaSegiempat.cpp
+++ Praktikum1/Keaktifan/24060124130064_AnggitaKiranaPuspa/LimaSegiempat.cpp
@@ -4,7 +4,7 @@
 // Lab: E2
 
 void SegiEmpat5() {
-	  glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0f, 0.0f, 0.0f); 
     glRectf(-0.2f,  0.1f, 0.4f, -0.1f);
     
@@ -14,7 +14,7 @@ void SegiEmpat5() {
     glColor3f(1.0f, 0.0f, 1.0f); 
     glRectf(-0.2f,  0.1f, -0.1f, -0.2f);
     
-	  glColor3f(1.0f, 1.0f, 1.0f);
+	glColor3f(1.0f, 1.0f, 1.0f);
     glRectf(-0.10f, -0.10,  0.10,  0.10);
 
     glColor3f(0.0f, 1.0f, 1.0f); 
