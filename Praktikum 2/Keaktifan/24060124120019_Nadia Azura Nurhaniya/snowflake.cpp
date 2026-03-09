f3978d7b87a04b5b10b7d2c6d1bbedff64f57d35
nadianurhaniya <nadianurhaniya@gmail.com>
2026-03-09 15:25:27 +0700

    update snowflake.cpp
    Updated snowflake drawing logic and display function.

--- Praktikum 2/Keaktifan/24060124120019_Nadia Azura Nurhaniya/snowflake.cpp
+++ Praktikum 2/Keaktifan/24060124120019_Nadia Azura Nurhaniya/snowflake.cpp
@@ -1,47 +1,42 @@
 // Nama : Nadia Azura Nurhaniya
 // NIM  : 24060124120019
-// Deskripsi : membuat Snowflakes dengan memanfaatkan rotasi
+// Deskripsi : membuat Snowflake dengan menggunakan rotasi
 
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <math.h>
 
 void Snowflake() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLineWidth(3.0f);
-    glColor3f(0.5f, 0.9f, 1.0f);
-
-    int numArms = 6; 
-    float mainLength = 0.5f; 
-    float branchLength = 0.12f; 
-
-    for (int i = 0; i < numArms; i++) {
+    for (int i = 0; i < 8; i++) {
         glPushMatrix();
-        glRotatef(i * 60.0f, 0,0,1); 
+        glRotated(i * 45.0, 0.0, 0.0, 1.0); 
         glBegin(GL_LINES);
-        glVertex2f(0,0); glVertex2f(0, mainLength);
-       
-        for(float p=0.2f; p<=mainLength; p+=0.15f) {
-            glVertex2f(0, p); glVertex2f(-branchLength, p + branchLength);
-            glVertex2f(0, p); glVertex2f(branchLength, p + branchLength);
-        }
-        
-        glVertex2f(0,0); glVertex2f(0.35f,0.35f);
-        glVertex2f(0,0); glVertex2f(-0.35f,0.35f);
+        glVertex2f(0.0f, 0.0f); 
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.0f, 0.25f); glVertex2f(0.12f, 0.35f);  
+        glVertex2f(0.0f, 0.25f); glVertex2f(-0.12f, 0.35f); 
+        glVertex2f(0.0f, 0.38f); glVertex2f(0.10f, 0.45f);  
+        glVertex2f(0.0f, 0.38f); glVertex2f(-0.10f, 0.45f); 
         glEnd();
         glPopMatrix();
     }
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLineWidth(5.0f);
+    glColor3f(0.7f, 0.85f, 0.9f); 
+    Snowflake(); 
     glFlush();
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
     glutInitWindowSize(640, 480);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
     glutCreateWindow("Snowflake");
-    glutDisplayFunc(Snowflake);
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); 
+    glutDisplayFunc(display);
+    glClearColor(0.3f, 0.1f, 0.1f, 1.0f); 
     glutMainLoop();
     return 0;
 }
